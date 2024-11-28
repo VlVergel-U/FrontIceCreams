f3b332ee8d0282b8696256b7cd0082fe16833a2a
Vale <vvergelv@ufpso.edu.co>
2024-11-27 22:08:15 -0500

    Actualizado

--- icecream/src/app/app.routes.ts
+++ icecream/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: LoginComponent },
+    { path: 'home', component: HomeComponent},
+    { path: '**', component: NotfoundComponent }
+
+];
